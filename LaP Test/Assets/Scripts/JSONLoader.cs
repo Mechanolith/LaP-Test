8485238ea71b97271ae4db8488ffc3b22c0e32b4
Mechanolith <themechanolith@gmail.com>
2017-12-12 20:09:21 +1000

    Created JSON Loader
    All required JSON data is now loaded into the game.

---
+++ LaP Test/Assets/Scripts/JSONLoader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+/// <summary>
+/// For loading all JSON in the game.
+/// </summary>
+public class JSONLoader : MonoBehaviour {
+
+	public JSONPayload payload;
+
+	private void Awake()
+	{
+		LoadJSON();
+	}
+
+	void Start () {
+		
+	}
+
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// Extracts the JSON from its file and puts it in its appropriate class.
+	/// </summary>
+	void LoadJSON()
+	{
+		FileInfo fInfo = new FileInfo(Application.dataPath + "/Provided Assets/myworld.ver1.words.json");
+		StreamReader reader = fInfo.OpenText();
+
+		string text;
+		string JSONText = "";
+
+		//Read all the text from the file and store it in jsonText.
+		do
+		{
+			text = reader.ReadLine();
+			JSONText += text;
+		}
+		while (text != null);
+		reader.Close();
+
+		payload = JsonUtility.FromJson<JSONPayload>(JSONText);
+	}
+}
---
+++ LaP Test/Assets/Scripts/JSONPayload.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class JSONPayload {
+	public List<Payload> payload = new List<Payload>();
+}
+
+[System.Serializable]
+public class Payload
+{
+	//public int id;
+	//public int view_order;
+	//public float offset;
+	//public float duration;
+	public string content;
+	public Audio audio;
+}
+
+[System.Serializable]
+public class Audio
+{
+	public string path;
+}
