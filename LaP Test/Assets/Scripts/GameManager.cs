b7f964a49159bf9481c654f8458b42c7018b8c14
Mechanolith <themechanolith@gmail.com>
2017-12-13 23:55:08 +1000

    Added Points, Word Death, Spawn Zoning, And Terrible Overlap Correction
    Clicking words now earns you a point.
    Points are saved and reloaded every time.
    
    Words now die after a set time.
    Missing a word will spawn a new one, with no sound.
    Words now only spawn in a specific area on the screen, avoiding the middle/edges to a given degree on each axis.
    Words now try to avoid overlapping each other. This system is really bad and will be replaced.

--- LaP Test/Assets/Scripts/GameManager.cs
+++ LaP Test/Assets/Scripts/GameManager.cs
@@ -4,27 +4,56 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+	[Header("General")]
 	[Tooltip("How long before the game starts (in seconds).")]
 	public float startDelay;
-	[Tooltip("The maximum number of words allowed on the screen at any one time.")]
-	public int maxWords;
-	[Tooltip("Increases the amount of time before audio clips are unloaded.")]
-	public float audioPadding;	//Some durations seem to be a bit short. Possibly an error in the JSON.
+	[Tooltip("The number of words that will be spawned.")]
+	public int wordsToSpawn;
+
+	[Header("Word Spawning")]
+	[Range(0f, 0.5f), Tooltip("Distance from the center (on the X axis) before words STOP spawning. Relative to screen size.")]
+	public float xScreenMax;
+	[Range(0f, 0.5f), Tooltip("Distance from the center (on the X axis) before words START spawning. Relative to screen size.")]
+	public float xScreenMin;
+	[Range(0f, 0.5f), Tooltip("Distance from the center (on the Y axis) before words STOP spawning. Relative to screen size.")]
+	public float yScreenMax;
+	[Range(0f, 0.5f), Tooltip("Distance from the center (on the Y axis) before words START spawning. Relative to screen size.")]
+	public float yScreenMin;
 	[Tooltip("The base word object that will be spawned.")]
 	public GameObject wordObject;
+
+	[Header("Audio")]
+	[Tooltip("Increases the amount of time before audio clips are unloaded.")]
+	public float audioPadding;	//Some durations seem to be a bit short. Possibly an error in the JSON.
+
 	Transform canvas;
 	JSONLoader jsonLoader;
 	AudioManager aMan;
-	int currentWords;
-	float maxBoundX;    //Min and Max bounds for spawns on the X axis.
-	float minBoundX;
-	float maxBoundY;    //Min and Max bounds for spawns on the Y axis.
-	float minBoundY;
+	int points;
 
 	void Start() {
 		canvas = GameObject.Find("Canvas").transform;
 		jsonLoader = GetComponent<JSONLoader>();
 		aMan = GetComponent<AudioManager>();
+
+		//Make sure our deadzones make sense. If not, make them the closest viable option (prioritising Min values).
+		if(xScreenMax < xScreenMin)
+		{
+			Debug.LogWarning("xScreenMax is smaller than xScreenMin. xScreenMax is now EQUAL to xScreenMin.");
+			xScreenMax = xScreenMin;
+		}
+
+		if(yScreenMax < yScreenMin)
+		{
+			Debug.LogWarning("yScreenMax is smaller than yScreenMin. yScreenMax is now EQUAL to yScreenMin.");
+			yScreenMax = yScreenMin;
+		}
+
+		//Load previous points.
+		if (PlayerPrefs.HasKey("Points"))
+		{
+			points = PlayerPrefs.GetInt("Points");
+		}
 	}
 
 	void Update() {
@@ -33,38 +62,76 @@ public class GameManager : MonoBehaviour {
 			startDelay -= Time.deltaTime;
 			if (startDelay <= 0f)
 			{
-				SpawnWord();
+				for (int indexA = 0; indexA < wordsToSpawn; ++indexA)
+				{
+					SpawnWord();
+				}
 			}
-		}
-		else
-		{
-
 		}
 	}
 
 	public void OnWordClick(Payload _wordInfo)
 	{
-		--currentWords;
 		aMan.PlaySound(_wordInfo.audio.path, _wordInfo.duration + audioPadding);
 		SpawnWord();
+		GetPoint();
+	}
+
+	public void OnWordMiss(GameObject _missedWord)
+	{
+		SpawnWord();
+		Destroy(_missedWord);
+	}
+
+	public void GetPoint()
+	{
+		++points;
+		
+		//Set UI counter.
+		//TODO
+
+		//Save it so it isn't lost.
+		PlayerPrefs.SetInt("Points", points);
+		PlayerPrefs.Save();
 	}
 
 	void SpawnWord()
 	{
-		if (currentWords < maxWords)
-		{
-			float xPos = Random.Range(minBoundX, maxBoundX);
-			float yPos = Random.Range(minBoundY, maxBoundY);
+		Payload wordToSpawn = GetRandomWord();
 
-			Vector3 spawnPos = new Vector3(xPos, yPos, 0f);
+		//Reduce the max X value relative to the word length (so things don't go off screen).
+		float xMax = xScreenMax;
+		//float minDist = 0.5f - (wordToSpawn.content.Length * 0.05f);
 
-			GameObject word = Instantiate(wordObject, spawnPos, Quaternion.identity);
-			word.transform.SetParent(canvas);
+		////If the word is unreasonably large, let it go into the inner deadzone.
+		//if(minDist < xScreenMin)
+		//{
+		//	xScreenMin = minDist;
+		//}
 
-			word.GetComponent<Word>().SetWord(GetRandomWord(), this);
+		//xMax = Mathf.Clamp(xMax, xScreenMin, minDist);
 
-			++currentWords;
-		}
+		//Find where within each band each co-ord sits.
+		float xPos = Random.Range(xMax, xMax);
+		float yPos = Random.Range(yScreenMin, yScreenMax);
+
+		//Randomise which band they're in.
+		float xMod = Random.Range(0f, 1f) < 0.5f ? 0f : 0.5f;
+		float yMod = Random.Range(0f, 1f) < 0.5f ? 0f : 0.5f;
+
+		//Debug.Log("Spawning word at screen pos " + (xPos + xMod) + ", " + (yPos + yMod));
+
+		//Put it in world space.
+		Vector3 screenVector = new Vector3(xPos + xMod, yPos + yMod, 0f);
+
+		Vector3 spawnPos = Camera.main.ViewportToWorldPoint(screenVector);
+		spawnPos.z = 0f;	//Make sure the z value is always consistent in world space.
+
+		//Spawn and setup the word.
+		GameObject word = Instantiate(wordObject, spawnPos, Quaternion.identity);
+		word.transform.SetParent(canvas);
+
+		word.GetComponent<Word>().SetWord(wordToSpawn, this);
 	}
 
 	Payload GetRandomWord()
--- LaP Test/Assets/Scripts/Word.cs
+++ LaP Test/Assets/Scripts/Word.cs
@@ -5,16 +5,39 @@ using TMPro;
 
 public class Word : MonoBehaviour {
 
+	[Tooltip("The amount of time (in seconds) before this word will automatically destroy itself.")]
+	public float deathTime = 5f;
+
+	[Tooltip("How far the word will move to get out of the way of something.")]
+	public float adjustmentValue;
+
 	TextMeshProUGUI textMesh;
 	Payload wordInfo;
 	GameManager gMan;
 
+	BoxCollider2D boxCol;
+	bool positionLocked = false;
+
 	void Awake () {
 		textMesh = GetComponentInChildren<TextMeshProUGUI>();
+		boxCol = GetComponent<BoxCollider2D>();
 	}
 	
 	void Update () {
-		
+		if(deathTime > 0f)
+		{
+			deathTime -= Time.deltaTime;
+
+			if(deathTime <= 0f)
+			{
+				gMan.OnWordMiss(gameObject);
+			}
+		}
+
+		if (!positionLocked)
+		{
+			CheckPosition();
+		}
 	}
 
 	public void OnClick()
@@ -46,7 +69,50 @@ public class Word : MonoBehaviour {
 		tempString = char.ToUpper(tempString[0]) + tempString.Substring(1);
 
 		textMesh.text = tempString;
-		
+
+		CheckPosition();
 		//Debug.Log(wordInfo.content + " = " + textMesh.text);
+	}
+
+	void CheckPosition()
+	{
+		//Set up the circle cast based on the text bounds.
+		Vector3 origin = gameObject.transform.position;
+		origin.x += boxCol.bounds.extents.x;
+		float radius = boxCol.bounds.extents.y;
+		float distance = boxCol.bounds.size.x;
+
+		RaycastHit2D[] hits = Physics2D.CircleCastAll(origin, radius, -transform.right, distance);
+
+		//If we hit anything (not including hitting yourself)...
+		if(hits.Length > 1)
+		{
+			Vector3 adjustVec = Vector3.zero;
+
+			//Adjust the position based on where the things we hit were.
+			for (int indexA = 0; indexA < hits.Length; ++indexA)
+			{
+				if (hits[indexA].transform != transform)	//Ignore hitting yourself.
+				{
+					//Vector2 posVec = new Vector2(transform.position.x, transform.position.y);
+					//Vector2 dirVec = posVec - hits[indexA].point;
+
+					//Vector3 directionVector = new Vector3(dirVec.x, dirVec.y, 0f);
+
+					Vector3 directionVector = transform.position - hits[indexA].transform.position;
+					adjustVec += directionVector.normalized * adjustmentValue;
+				}
+			}
+
+			transform.position += adjustVec;
+
+			Debug.Log("Adjusting by " + adjustVec);
+		}
+		else
+		{
+			//Otherwise we're good. Stop trying to change things.
+			positionLocked = true;
+			Debug.Log("Hit nothing, position locked!");
+		}
 	}
 }
